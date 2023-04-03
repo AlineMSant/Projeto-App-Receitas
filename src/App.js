e31e00864ac4a89d22884f16b7190b2896eab03e
AlineMSant <alinems4120@gmail.com>
2023-04-03 17:07:20 -0300

    requisito 4, inputs e lógica para context concluido

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-import NotFound from './pages/NotFound';
+// import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="*" component={ NotFound } />
+      {/* <Route path="*" component={ NotFound } /> */}
 
     </Switch>
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+
 
 function Login() {
+  const { email, setEmail, password, setPassword } = useContext(AppContext);
+
   return (
     <div>
       <input
         type="text"
         data-testid="email-input"
+        onChange={ (e) => setEmail(e.target.value) }
       />
 
       <input
         type="password"
         data-testid="password-input"
+        onChange={ (e) => setPassword(e.target.value) }
+
       />
 
       <button
@@ -24,3 +31,5 @@ function Login() {
 }
 
 export default Login;
+
+// requisitos 2-3: Aline, Nicola, Samara, Camila, Patrick
