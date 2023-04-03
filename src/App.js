e38a3b61a0984390103ccca68d2c576877bd6740
AlineMSant <alinems4120@gmail.com>
2023-04-03 16:41:21 -0300

    requisito 2 e 3 concluido

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Switch>
-      <Route />
+      <Route exact path="/" component={ Login } />
+      <Route path="*" component={ NotFound } />
 
     </Switch>
 
---
+++ src/pages/Login.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <input
+        type="text"
+        data-testid="email-input"
+      />
+
+      <input
+        type="password"
+        data-testid="password-input"
+      />
+
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </div>
+  );
+}
+
+export default Login;
