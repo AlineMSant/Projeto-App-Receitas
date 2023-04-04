f7df3f81b1314d65631914d26c6a998061b3d16c
AlineMSant <alinems4120@gmail.com>
2023-04-04 18:40:22 -0300

    deleta arquivos mocks

--- src/tests/helpers/mockFetch.js
+++ src/tests/helpers/mockFetch.js
@@ -1,16 +1,9 @@
-const mockMeals = require('./mockMeals');
-const mockMealsCategories = require('./mockMealCategories');
+// const mockMeals = require('./mockMeals');
 
 const mockFetch = (url) => {
   if (url === 'https://www.themealdb.com/api/json/v1/1/search.php?s=') {
     return Promise.resolve({
       json: () => Promise.resolve(mockMeals),
-    });
-  }
-
-  if (url === 'https://www.themealdb.com/api/json/v1/1/list.php?c=list') {
-    return Promise.resolve({
-      json: () => Promise.resolve(mockMealsCategories),
     });
   }
 };
