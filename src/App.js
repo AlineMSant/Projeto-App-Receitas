ce0fb477e94607bc59fcfef95c81ba709451d91b
ptkramos <pdsr96@gmail.com>
2023-04-04 10:49:59 -0300

    Requisitos de 7 a 9 concluídos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 // import NotFound from './pages/NotFound';
 
 function App() {
@@ -11,6 +15,11 @@ function App() {
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route path="/meals" component={ Meals } />
+      <Route path="/drinks" component={ Drinks } />
+      {/* Manter Meals e Drinks, excluir recipes */}
+      <Route path="/profile" component={ Profile } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
+      <Route path="/favorite-recipes" component={ FavoriteRecipes } />
 
       {/* <Route path="*" component={ NotFound } /> */}
 
---
+++ src/components/Header.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import ProfileIcon from '../images/profileIcon.svg';
+import SearchIcon from '../images/searchIcon.svg';
+
+function Header() {
+  const route = useLocation();
+  const history = useHistory();
+  const [showSearchInput, setShowSearchInput] = useState(false);
+
+  function getPageTitle(pathname) {
+    switch (pathname) {
+    case '/meals':
+      return 'Meals';
+    case '/drinks':
+      return 'Drinks';
+    case '/profile':
+      return 'Profile';
+    case '/done-recipes':
+      return 'Done Recipes';
+    case '/favorite-recipes':
+      return 'Favorite Recipes';
+    default:
+      return 'Recipes App';
+    }
+  }
+
+  const title = getPageTitle(route.pathname);
+
+  const renderSearchBtn = ![
+    '/profile',
+    '/done-recipes',
+    '/favorite-recipes',
+  ].includes(route.pathname);
+
+  return (
+    <>
+      <div>
+        <button
+          type="button"
+          onClick={ () => history.push('/profile') }
+        >
+          <img src={ ProfileIcon } alt="Profile Icon" data-testid="profile-top-btn" />
+        </button>
+
+        <span
+          data-testid="page-title"
+        >
+          { title }
+        </span>
+
+        { renderSearchBtn && (
+          <button
+            type="button"
+            onClick={ () => setShowSearchInput(!showSearchInput) }
+          >
+            <img src={ SearchIcon } alt="Search Icon" data-testid="search-top-btn" />
+          </button>
+        ) }
+      </div>
+
+      <div>
+        { showSearchInput && (
+          <input
+            data-testid="search-input"
+            type="text"
+          />
+        ) }
+      </div>
+    </>
+  );
+}
+
+export default Header;
