7c7ecb1b42e0856acf3b77cdee8c700f1728c0d9
AlineMSant <alinems4120@gmail.com>
2023-04-06 10:41:55 -0300

    requisitos 57-58-59 concluidos

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,9 +3,35 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const emailLocalStorage = JSON.parse(localStorage.getItem('user')).email;
+
   return (
     <div>
       <Header />
+
+      <h4 data-testid="profile-email">{emailLocalStorage}</h4>
+
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
+
       <Footer />
     </div>
   );
